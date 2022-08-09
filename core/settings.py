9521349ca6614d3d318859bb5fb4c04cc8f8b560
sebastian <se.olivaresc@gmail.com>
2022-08-08 23:52:16 -0400

    el mensaje

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+# DEBUG = env.bool("DEBUG", default=False)
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = 'RENDER' not in os.environ
 
--- render/build.sh
+++ render/build.sh
@@ -3,6 +3,8 @@
 # Exit on error
 set -o errexit
 
+python -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
